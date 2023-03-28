0ff87de70a0de01f774c679e151aaa34f38e3dcc
Hants Williams <hantsawilliams@gmail.com>
2023-03-28 10:18:35 -0400

    added in endpoints

--- Class_8/basicAPI/app.py
+++ Class_8/basicAPI/app.py
@@ -3,9 +3,47 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
+# endpoints 
 @app.route('/')
 def default_home():
     return 'API Endpoint from Hants Williams'
 
-# if __name__ == '__main__':
-#     app.run(debug=True, host='localhost', port=80)
+#### RETRIEVING 
+
+@app.route('/api/v1/students')
+def students():
+    df = pd.read_csv('datafile.csv')
+    return df.to_json(orient='records')
+
+@app.route('/api/v1/students/<int:limit>')
+def students_limit(limit):
+    df = pd.read_csv('datafile.csv')
+    df = df.head(limit)
+    return df.to_json(orient='records')
+
+
+
+
+##### SEARCHING #####
+
+@app.route('/api/v1/students/search/lastname/<string:lastname>')
+def students_search_lastname(lastname):
+    df = pd.read_csv('datafile.csv')
+    df.columns = df.columns.str.replace(' ', '_')
+    df = df[df['Last_Name'].str.contains(lastname)]
+    return df.to_json(orient='records')
+
+@app.route('/api/v1/students/search/firstname/<string:firstname>')
+def students_search_firstname(firstname):
+    df = pd.read_csv('datafile.csv')
+    df.columns = df.columns.str.replace(' ', '_')
+    df = df[df['First_Name'].str.contains(firstname)]
+    return df.to_json(orient='records')
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=80)
